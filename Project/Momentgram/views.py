c326742df4bfe5ec2e8f688390301dcf77f4972f
paionxl <maestro-17653@hotmail.com>
2019-09-30 12:21:30 +0200

    login y registro implementados en backend

--- Project/Momentgram/views.py
+++ Project/Momentgram/views.py
@@ -1,14 +1,23 @@
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 
 
 def index(request):
     return render(request, 'Momentgram/register.html')
 
 def signUp(request):
-    username = request.POST.get('Name')
-    email = request.POST.get('email')
-    password = request.POST.get('psw')
-    user = User.objects.create_user(username, email, password)
-    return HttpResponse("Welcome" + email + "to Momentgram")
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = User.objects.create_user(username, email, password)
+        return HttpResponse("Welcome" + email + "to Momentgram")
+
+def signIn(request):
+    if request.method == 'GET':
+        username = request.GET.get('username')
+        password = request.GET.get('password')
+        user = authenticate(username, password)
+        return user # user if good, None if bad
