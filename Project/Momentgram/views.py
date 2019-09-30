c90a13ba93e84f02b5c3facfc647633ad164ef08
paionxl <maestro-17653@hotmail.com>
2019-09-30 11:40:45 +0200

    reverted firebase connection

--- Project/Momentgram/views.py
+++ Project/Momentgram/views.py
@@ -1,26 +1,14 @@
 from django.http import HttpResponse
-import pyrebase
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
-config = {
-    'apiKey': "AIzaSyCQRNHU0SMwMws_Zt5NZCR0q0XOTL5MY9M",
-    'authDomain': "momentgram-7f5a3.firebaseapp.com",
-    'databaseURL': "https://momentgram-7f5a3.firebaseio.com",
-    'projectId': "momentgram-7f5a3",
-    'storageBucket': "momentgram-7f5a3.appspot.com",
-    'messagingSenderId': "903804373398",
-    'appId': "1:903804373398:web:60bd1d367411d20d63f5b7"
-}
-
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
 
 def index(request):
     return render(request, 'Momentgram/register.html')
 
 def signUp(request):
+    username = request.POST.get('Name')
     email = request.POST.get('email')
     password = request.POST.get('psw')
-
-    auth.create_user_with_email_and_password(email, password)
+    user = User.objects.create_user(username, email, password)
     return HttpResponse("Welcome" + email + "to Momentgram")
