477edff6eed2671891a7d43e7f87bfcc96aacf43
paionxl <maestro-17653@hotmail.com>
2019-10-07 11:50:00 +0200

    Faltan cambios en html register y login

--- Project/Momentgram/urls.py
+++ Project/Momentgram/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^init/$', views.init, name='init'),
-    url(r'^done/$', views.register, name='register')
-]
+    url(r'^$', views.init, name='init'),
+    url(r'^login/$', views.signIn, name='login'),
+    url(r'^signup/$', views.register, name='signup'),
+    url(r'^signup/done/$', views.register, name='doner'),
+    url(r'^login/done/$', views.signIn, name='donel')
 
+]
--- Project/Momentgram/views.py
+++ Project/Momentgram/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
+
 
 
 
@@ -16,24 +17,25 @@ def register(request):
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
-        if User.objects.filter(username=username, email=email).exists():
-            return HttpResponse("Username: " + username + " in use. Please try another one.")
+        if User.objects.filter(username=username).exists() or User.objects.filter(email = email).exists():
+            return HttpResponse("Username: " + username + "or mail: " + email +  " in use. Please try another one.")
         else:
             user = User.objects.create_user(username, email, password)
             return HttpResponse("Welcome to Momentgram, " + user.username)
     return render(request, 'Momentgram/register.html')
 
 def signIn(request):
-    if request.method == 'GET':
-        username = request.GET.get('username')
-        password = request.GET.get('password')
-        user = authenticate(username, password)
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password = password)
         if user:
-            login(user)
-        return user # user if good, None if bad
-
+            login(request, user = user)
+            return HttpResponse("logged in")
+        else:
+            return HttpResponse("Failed. Username or password not correct")
+    return render(request, 'Momentgram/login.html')
 
 def init(request):
-
     return render(request, 'Momentgram/init.html')
 
