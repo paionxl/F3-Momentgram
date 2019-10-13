f71559ec099ddb4bd7054e7ae1f8ddbcab7cb405
lingzhu1997 <lingzhu111@hotmail.com>
2019-10-13 23:19:50 +0200

    Issue F-104 done

--- Project/Momentgram/urls.py
+++ Project/Momentgram/urls.py
@@ -6,16 +6,14 @@ from django.conf import settings # new
 from django.urls import path, include # new
 from django.conf.urls.static import static # new
 
+
+
 urlpatterns = [
-
-    url(r'^$', views.index, name='init'),
+    url(r'^$', views.index, name='index'),
+ 	url(r'^entry/$', views.entry, name='entry' ),
+    url(r'^register/$', views.register, name='register'),
     url(r'^login/$', views.signIn, name='login'),
-    url(r'^signup/$', views.register, name='signup'),
-    url(r'^signup/done/$', views.register, name='doner'),
-    url(r'^login/done/$', views.signIn, name='donel')
-    #path('admin/', admin.site.urls),
-    #path('', include('posts.urls'))
-
+    url(r'^logout/$', views.logout, name='logout')
 ]
 
 #if settings.DEBUG: # new
--- Project/Momentgram/views.py
+++ Project/Momentgram/views.py
@@ -4,19 +4,17 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
-
-from django.contrib.auth import authenticate, login
-
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 
 
 def index(request):
     return render(request, 'Momentgram/init.html')
 
 @login_required
-def entry(reques):
+def entry(request):
     return render(request, 'Momentgram/entry.html')
 
 
@@ -29,21 +27,31 @@ def register(request):
             return HttpResponse("Username: " + username + "or mail: " + email +  " in use. Please try another one.")
         else:
             user = User.objects.create_user(username, email, password)
-            return HttpResponse("Welcome to Momentgram, " + user.username)
+            return HttpResponseRedirect(reverse("signIn"))
     return render(request, 'Momentgram/register.html')
+
+
 
 def signIn(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-
-        user = authenticate(username=username, password = password)
+        user = authenticate(username= username, password=password)
+        
         if user:
-            login(request, user = user)
-            return HttpResponse("logged in")
+            if user.is_active:
+                login(request,user)
+                return HttpResponseRedirect(reverse('entry'))
+            else:
+                return HttpResponse("Your account was inactive.")
         else:
-            return HttpResponse("Failed. Username or password not correct")
+            print("Someone tried to login and failed.")
+            print("They used username: {} and password: {}".format(username,password))
+            return HttpResponse("Invalid login details given")
+       
+     
     return render(request, 'Momentgram/login.html')
 
-
+def logout(request):
+    return redirect('/')
 
