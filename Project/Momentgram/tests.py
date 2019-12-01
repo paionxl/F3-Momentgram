b803e74373afd47abc387f40c75c8187280accdc
Albert Solà Roca <albert.sola9@gmail.com>
2019-12-01 09:54:57 +0100

    Added some tests for messages
    For now we test that the message content is saved correctly in the database, when utils gets uploaded to dev I will also check that we get the correct number of messages with a query

--- Project/Momentgram/tests.py
+++ Project/Momentgram/tests.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
-from Momentgram.models import Post
+from Momentgram.models import Post, Message
 
 
 # Create your tests here.
@@ -26,7 +26,7 @@ class ProfileTestCase(TestCase):
         return self.assertFalse(self.client.login(username="Albert", password=""))
 
     def test_profile_content(self):
-        user = User.objects.get(id=1)
+        user = User.objects.get(username="Albert")
         expected_user_name = f'{user.username}'
         self.assertEqual(expected_user_name,"Albert")
 
@@ -36,7 +36,7 @@ class PostTestCase(TestCase):
         User.objects.create_user("Albert", "albert.sola9@gmail.com", "admin1234")
         Post.objects.create(description="test description",
                             image="media/images/GoldenGateBridge_BakerBeach_MC.jpg",
-                            user=User.objects.get(id=1))
+                            user=User.objects.get(username="Albert"))
 
     #Returns correct content after uploading a post
     def test_post_content(self):
@@ -59,3 +59,33 @@ class FollowTestCase(TestCase):
         User.objects.create_user("Carlos", "carlos_arenas@gmail.com", "esfw")
 
     #TODO tests with followers and following
+
+
+class MessagesTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user("Albert", "albert.sola9@gmail.com", "admin1234")
+        User.objects.create_user("Manuel", "manuel.lecha@gmail.com", "manu")
+        Message.objects.create(sender= User.objects.get(username="Albert"),
+                               receiver= User.objects.get(username="Manuel"),
+                               text= "Holaaa :D")
+        Message.objects.create(sender=User.objects.get(username="Manuel"),
+                               receiver=User.objects.get(username="Albert"),
+                               text="Hello!")
+        Message.objects.create(sender=User.objects.get(username="Manuel"),
+                               receiver=User.objects.get(username="Albert"),
+                               text="Porque no contestas D:")
+        Message.objects.create(sender=User.objects.get(username="Manuel"),
+                               receiver=User.objects.get(username="Albert"),
+                               text="xd")
+
+    def test_message_content(self):
+        message = Message.objects.get(sender=User.objects.get(username="Albert"))
+        expected_text = f'{message.text}'
+        expected_receiver = f'{message.receiver}'
+        expected_sender = f'{message.sender}'
+        self.assertEqual(expected_sender,"Albert")
+        self.assertEqual(expected_receiver,"Manuel")
+        self.assertEqual(expected_text,"Holaaa :D")
+
+    def test_number_of_messages(self):
+        #TODO when utils is uploaded
