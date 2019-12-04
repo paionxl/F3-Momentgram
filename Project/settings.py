3b5f9b558968cdcea4c2df16718c7103226b06bb
paionxl <maestro-17653@hotmail.com>
2019-12-04 17:40:54 +0100

    images

--- Project/settings.py
+++ Project/settings.py
@@ -125,7 +125,7 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
