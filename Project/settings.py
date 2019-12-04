1efda2c7845784ab6d6e90722e612ca8cf6feab8
paionxl <maestro-17653@hotmail.com>
2019-12-04 17:21:50 +0100

    images

--- Project/settings.py
+++ Project/settings.py
@@ -91,10 +91,6 @@ DATABASES = {
     }
 }
 
-#media
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_URL = 'login'
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -130,3 +126,6 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
