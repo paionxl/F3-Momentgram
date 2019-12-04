11ca0a24a9cd247cb9f449fcffe669028f8d938e
paionxl <maestro-17653@hotmail.com>
2019-12-04 17:29:02 +0100

    images

--- Project/settings.py
+++ Project/settings.py
@@ -126,6 +126,6 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
